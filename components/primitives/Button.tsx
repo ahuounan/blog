39d84da8b4a65f54184585addaa1db1ed307aca7
Alan Hu <on.alanhu@gmail.com>
2021-01-10 11:08:01 +0000

    feat: add primitives

--- components/primitives/Button.tsx
+++ components/primitives/Button.tsx
@@ -4,6 +4,5 @@ export const Button = styled('button', {
   padding: 0,
   background: 'transparent',
   border: 'none',
-  outline: 'none',
   cursor: 'pointer'
 });
---
+++ components/primitives/Link.tsx
@@ -0,0 +1,5 @@
+import LinkRaw from 'next/link';
+
+import { styled } from 'stitches.config';
+
+export const Link = styled(LinkRaw, {});
